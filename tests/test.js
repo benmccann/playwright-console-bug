99b7e97f40e244a61635cdaa4b943b4b86c58d08
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-11-26 19:46:27 -0800

    update test to use wait with predicate

--- tests/test.js
+++ tests/test.js
@@ -1,29 +1,21 @@
 import { expect, test } from '@playwright/test';
 
-test('using window.fetch causes a warning', async ({ page }) => {
-	/** @type {string[]} */
-	const warnings = [];
-
-	page.on('console', (msg) => {
-		if (msg.type() === 'warning') {
-			warnings.push(msg.text());
-		}
-	});
-
-	await page.goto('/load/window-fetch/incorrect');
+test('using window.fetch causes a warning', async ({ page, baseURL }) => {
+	await Promise.all([
+		page.goto('/load/window-fetch/incorrect'),
+		page.waitForEvent('console', {
+			predicate: (message) => {
+				const text = message.text();
+				if (
+					text ===
+					`Loading ${baseURL}/load/window-fetch/data.json using \`window.fetch\`. For best results, use the \`fetch\` that is passed to your \`load\` function: https://kit.svelte.dev/docs/load#making-fetch-requests`
+				) {
+					return true;
+				}
+				return false;
+			},
+			timeout: 3_000
+		})
+	]);
 	expect(await page.textContent('h1')).toBe('42');
-
-	const { origin } = new URL(page.url());
-	expect(warnings).toContain(
-		`Loading ${origin}/load/window-fetch/data.json using \`window.fetch\`. For best results, use the \`fetch\` that is passed to your \`load\` function: https://kit.svelte.dev/docs/load#making-fetch-requests`
-	);
-
-	warnings.length = 0;
-
-	await page.goto('/load/window-fetch/correct');
-	expect(await page.textContent('h1')).toBe('42');
-
-	expect(warnings).not.toContain(
-		`Loading ${origin}/load/window-fetch/data.json using \`window.fetch\`. For best results, use the \`fetch\` that is passed to your \`load\` function: https://kit.svelte.dev/docs/load#making-fetch-requests`
-	);
 });
